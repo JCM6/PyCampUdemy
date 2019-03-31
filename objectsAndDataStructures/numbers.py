e38d9aecfe82e7fd4fe1301c3616f3593f156e3b
JCM6 <jcmoody6@gmail.com>
2019-03-30 22:23:44 -0700

    Create numbers.py

---
+++ objectsAndDataStructures/numbers.py
@@ -0,0 +1,12 @@
+numbers = 6+6+6+6+6
+numbers2 = 1/2
+a = 78
+sliceOfLife = 'something fun that is a string'
+
+myIncome = 100
+taxRate = 0.1
+
+taxes = myIncome * taxRate
+print(taxes)
+print(sliceOfLife)
+print(len(sliceOfLife))
